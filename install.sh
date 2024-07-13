f1f2dd11781c7e688b481c5efc41c126404c9661
Xblaze <134009745+XblazeDEV@users.noreply.github.com>
2024-07-13 10:12:20 -0500

    Fixed install.sh program

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/usr/local/bin
 echo "Done"
 
 echo "Installing..."
-wget -q0 /usr/local/bin/test https://github.com/XblazeDEV/Test/releases/latest/download/test
+wget -q0 /usr/local/bin/test https://github.com/XblazeDEV/Test/releases/tag/test
 chmod +x /usr/local/bin/test
 
 echo "Updating dependencies..."
